77e5c39735ad7e0f2a19d3a3170052f4111f7990
Voicuu <andrei.voicu2@student.upt.ro>
2022-07-17 13:42:05 +0300

    Updates

---
+++ precache-manifest.1a5b67d6bde147e0b8f155186daa593f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "625cd6aec1fba58dcb0a",
+    "url": "/robofriends/static/css/main.93b885fc.chunk.css"
+  },
+  {
+    "revision": "625cd6aec1fba58dcb0a",
+    "url": "/robofriends/static/js/main.625cd6ae.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/robofriends/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "f56cdd88c44d6500b8fe",
+    "url": "/robofriends/static/css/2.f0518411.chunk.css"
+  },
+  {
+    "revision": "f56cdd88c44d6500b8fe",
+    "url": "/robofriends/static/js/2.f56cdd88.chunk.js"
+  },
+  {
+    "revision": "a433ede44710983729ce",
+    "url": "/robofriends/static/js/runtime~main.a433ede4.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "378a11ba51f85c5ce6b5285eeb6c8d6c",
+    "url": "/robofriends/index.html"
+  }
+];
